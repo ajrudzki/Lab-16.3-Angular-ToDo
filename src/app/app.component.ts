86e58c0ec2f9fa0d72015c0244bf654e8f0d09f2
Aaron Rudzki <aaronrudzki@quickenloans.com>
2020-10-08 19:24:05 -0400

    initial commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,50 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'LabAngularToDo';
+  title = 'TODO List';
+ 
+  toDoList: List[] = [
+    {
+      task: 'Fold Clothes',
+      complete: false
+    },
+    {
+      task: 'Put Clothes In Dresser',
+      complete: false
+    },
+    {
+      task: 'relax',
+      complete: false
+    },
+    {
+      task: 'Try To Pet Cat',
+      complete: true
+    },
+    {
+      task: 'Pet Dog',
+      complete: false
+    },
+    {
+      task: 'Be Awesome',
+      complete: false
+    }
+  ];
+
+  addTask(newtask: string){
+    this.toDoList.push({task: newtask, complete: false});
+  }
+
+  completeTask(iscomplete: List){
+    iscomplete.complete = true;
+  }
+    
+  removeTask(remove: List){
+    this.toDoList = this.toDoList.filter(item=>item!==remove);
+    console.log("you finished your task")
+  }  
 }
+
+interface List {
+  task: string,
+  complete: boolean
+};
